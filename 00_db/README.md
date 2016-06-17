f80801fc2a44827cd0796560a7bf6d18cc137613
Saupe Stefan <SSaupe@intern.cube.ch>
2016-06-17 16:05:21 +0200

    NOTICKET

--- 00_db/README.md
+++
@@ -1,6 +0,0 @@
-https://hub.docker.com/r/sath89/oracle-12c/
-
-To start the database run:
-docker run -d -p 1521:1521 -v /Users/stefan/Documents/docker/syrdock/00_db/data:/u01/app/oracle sath89/oracle-12c
-
-
--- 02_1221-domain/container-scripts/add-server.py
+++ 02_1221-domain/container-scripts/add-server.py
@@ -75,6 +75,41 @@ cmo.setEnabled(false)
 # -----------------------------------------------------------------------------------------
 cd('/Servers/' + msname + '/ServerStart/' + msname)
 arguments = '-Djava.security.egd=file:/dev/./urandom -Dweblogic.Name=' + msname + ' -Dweblogic.management.server=http://' + admin_host + ':' + admin_port + ' ' + memargs
+arguments = arguments + ' -Xmx3000M -Dsyrius.log4j.configuration.watch_interval=5000 \
+                          -Dsyrius.log4j.configuration.reconfigure=true \
+						  -Dsyrius.profiling=true \
+						  -Dsyrius.WebserviceEndpointConfig=file:///u01/oracle/syrius/config/jee_EndpointConfig.xml \
+						  -Dcom.sun.xml.namespace.QName.useCompatibleSerialVersionUID=1.0 \
+						  -Dsyrius.default_userid=SyriusAdmin \
+						  -Dsyrius.urldef.placeholder.conf=/u01/oracle/syrius/config/syrius.urldef.conf \
+						  -XX:+UnlockCommercialFeatures \
+						  -XX:+FlightRecorder  \
+						  -Dsyrius.searchengine_clustername=syriusEsCluster_HEAD_HANGAR_A \
+						  -Dsyrius.searchengine_tcptransportport=23417 \
+						  -Dlogstreamer.profiles=/u01/oracle/LogStreamerProfiles.xml \
+						  -Dlogstreamer.jgroups_config=file:///u01/oracle/syrius/config/fl_jgroups.xml \
+						  -Dlogstreamer.jgroups_name=HEAD_HANGAR_A \
+						  -Dlogstreamer.name=HEAD_HANGAR_A_test \
+						  -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=23411,server=y,suspend=n \
+						  -Dhttps.proxySet=true \
+						  -Dhttps.proxyHost=172.21.131.12 \
+						  -Dhttps.proxyPort=8080 \
+						  -Dsyrius.ldap.config=file:////u01/oracle/syrius/config/activeDirectory.conf \
+						  -Djava.security.auth.login.config=/u01/oracle/syruser-auth-AD.conf \
+						  -Dsyrius.default_userid=SyriusAdmin \
+						  -XX:+UseCompressedOops \
+						  -Duser.language=de \
+						  -Duser.region=CH \
+						  -Duser.timezone=Europe/Zurich \
+						  -Djava.net.preferIPv4Stack=true \
+						  -Dsyrius.log4j.reconfigure=true -Djava.util.Arrays.useLegacyMergeSort=true \
+						  -Dsun.lang.ClassLoader.allowArraySyntax=true \
+						  -Dsyrius.jta.TransactionManagerName=javax.transaction.TransactionManager \
+						  -Dsyrius.help.useprocessdef=true \
+						  -Dsyrius.auditlogger=/u01/oracle/syrius/config/auditlog.xml \
+						  -Dlog4j.configuration=/u01/oracle/syrius/config/jee_log4j.xml \
+						  -Dsyrius.cache.jgroups.config=file:/u01/oracle/syrius/config/jee_jgroups_conf.xml \
+						  -Ddb_type=oracle'
 cmo.setArguments(arguments)
 saveActivate()
 
