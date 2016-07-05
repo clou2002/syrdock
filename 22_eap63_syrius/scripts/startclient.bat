a179636d188226430904acd9a53a5b8daedb3b8b
Stefan Saupe <clou2002@users.noreply.github.com>
2016-07-05 13:58:15 +0200

    moved helper scripts to scripts directory

---
+++ 22_eap63_syrius/scripts/startclient.bat
@@ -0,0 +1,1 @@
+java -jar ..\syrius\software\Executables\ASE-Client\jps-client-boot-6.7.31.jar --host 192.168.99.100 --port 42703
---
+++ 22_eap63_syrius/scripts/startclient.sh
@@ -0,0 +1,1 @@
+java -jar ../syrius/software/Executables/ASE-Client/jps-client-boot-6.7.31.jar --host localhost --port 42703
