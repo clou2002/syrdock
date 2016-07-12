ef97a2a00cca6ff6fa13163122a9b9e29f153550
clou2002 <stefan@saupe.ch>
2016-07-12 13:17:09 +0200

    23 - syrius sw outside of image

---
+++ 23_eap63_syrius_volume/scripts/start_utc.sh
@@ -0,0 +1,1 @@
+docker run -d --name syr_utc --link syr_app:syrnet -p 42703:42703 clou/syr:3_06_HEAD /opt/jboss/syrius/bin/utc_start
---
+++ 23_eap63_syrius_volume/scripts/startclient.sh
@@ -0,0 +1,1 @@
+java -jar ../syrius/software/Executables/ASE-Client/jps-client-boot-*.jar --host localhost --port 42703 --client-type swing
