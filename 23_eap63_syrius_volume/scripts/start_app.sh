542445f70f5bd460b4191a154ffe4982e1f42156
clou2002 <clou2002@users.noreply.github.com>
2016-08-23 15:38:24 +0200

    running volume example

--- 23_eap63_syrius_volume/scripts/start_app.sh
+++ 23_eap63_syrius_volume/scripts/start_app.sh
@@ -1,5 +1,8 @@
 docker run -d --name syr_app \
---volume /c/Users/SSaupe/docker-workspace/ssa/syrdock/23_eap63_syrius_volume/syrius:/opt/jboss/syrius:ro \
---volume /c/Users/SSaupe/docker-workspace/ssa/syrdock/23_eap63_syrius_volume/syrius/logs:/opt/jboss/syrius/logs \
---volume /c/Users/SSaupe/docker-workspace/ssa/syrdock/23_eap63_syrius_volume/syrius/software/Executables/ASE-J2EE-Server/ear:/opt/jboss/jboss-eap-6.3/standalone/deployments:ro \
+--volume /Users/stefan/Documents/docker/syrdock/23_eap63_syrius_volume/syrius/properties:/opt/jboss/syrius/properties:ro \
+--volume /Users/stefan/Documents/docker/syrdock/23_eap63_syrius_volume/syrius/config:/opt/jboss/syrius/config:ro \
+--volume /Users/stefan/Documents/docker/syrdock/23_eap63_syrius_volume/syrius/config/standalone-full-syrius.xml:/opt/jboss/jboss-eap-6.3/standalone/configuration/standalone-full-syrius.xml:ro \
+--volume /Users/stefan/Documents/docker/syrdock/23_eap63_syrius_volume/syrius/logs:/opt/jboss/syrius/logs \
+--volume /Users/stefan/Documents/docker/syrdock/23_eap63_syrius_volume/syrius/software/Executables/ASE-J2EE-Server/ear/syrius_services.ear:/opt/jboss/jboss-eap-6.3/standalone/deployments/syrius_services.ear:ro \
+--volume /Users/stefan/Documents/docker/syrdock/23_eap63_syrius_volume/syrius/software/Executables/ASE-UTC-Server:/opt/jboss/syrius/ASE-UTC-SERVER/:ro \
 -p 42705:42705 -p 42707:42707 -p 9990:9990 clou/syr:3_06_HEAD_vol
--- 23_eap63_syrius_volume/scripts/start_utc.sh
+++ 23_eap63_syrius_volume/scripts/start_utc.sh
@@ -1,1 +1,6 @@
-docker run -d --name syr_utc --link syr_app:syrnet -p 42703:42703 clou/syr:3_06_HEAD /opt/jboss/syrius/bin/utc_start
+docker run -d --name syr_utc --link syr_app:syrnet -p 42703:42703 \
+--volume /Users/stefan/Documents/docker/syrdock/23_eap63_syrius_volume/syrius/properties:/opt/jboss/syrius/properties:ro \
+--volume /Users/stefan/Documents/docker/syrdock/23_eap63_syrius_volume/syrius/config:/opt/jboss/syrius/config:ro \
+--volume /Users/stefan/Documents/docker/syrdock/23_eap63_syrius_volume/syrius/logs:/opt/jboss/syrius/logs \
+--volume /Users/stefan/Documents/docker/syrdock/23_eap63_syrius_volume/syrius/software/Executables/ASE-UTC-Server:/opt/jboss/syrius/ASE-UTC-Server/:ro \
+clou/syr:3_06_HEAD_vol /opt/jboss/syrius/bin/utc_start
